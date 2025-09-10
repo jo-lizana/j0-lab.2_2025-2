d0973457091860c58dd7c34ca3633f393fe29376
jo-lizana <josefa.lizana@uniacc.edu>
2025-09-10 00:03:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+import data from 'SOLEMNE-1/tcc_ceds_music.json' with {type : 'json'}
+
+const canciones = [
+      { artist_name: "mukesh", track_name: "mohabbat bhi jhoothi", release_date: 1950, genre: "pop", topic: "sadness" },
+      { artist_name: "madonna", track_name: "like a virgin", release_date: 1985, genre: "pop", topic: "romantic" },
+      { artist_name: "ariana grande", track_name: "break free", release_date: 2014, genre: "pop", topic: "dance" },
+      { artist_name: "billie eilish", track_name: "bad guy", release_date: 2019, genre: "pop", topic: "rebellious" }
+    ];
+
